1fb1d561cbb1c1ad9f3552fc9942933365e3bc44
kvsd@fw21z <f.barbarino@gmail.com>
2014-01-28 10:57:41 +0100

    Inserite nuove query.
    Ora e' possibile gestire le tabelle marca, cat_merce, unita misura,
    cod iva, e sede magazzino

--- utils/simpledialogs.h
+++ utils/simpledialogs.h
@@ -14,6 +14,11 @@ const QString ADD_CITTA_QUERY = "INSERT INTO citta(descr) VALUES(:value)";
 const QString ADD_PROVINCIA_QUERY = "INSERT INTO provincia(descr) VALUES(:value)";
 const QString ADD_CAP_QUERY = "INSERT INTO cap(descr) VALUES(:value)";
 const QString ADD_STATO_QUERY = "INSERT INTO stato(descr) VALUES(:value)";
+const QString ADD_MARCA_QUERY = "INSERT INTO marca(descr) VALUES(:value)";
+const QString ADD_CAT_MERCE_QUERY = "INSERT INTO cat_merce(descr) VALUES(:value)";
+const QString ADD_UM_QUERY = "INSERT INTO unita_misura(descr) VALUES(:value)";
+const QString ADD_IVA_QUERY = "INSERT INTO cod_iva(descr) VALUES(:value)";
+const QString ADD_SEDE_QUERY = "INSERT INTO sede_magazzino(descr) VALUES(:value)";
 
 void allDlg(QWidget *parent=0, QSqlTableModel *model=0, QString strquery="", const char *title="", QString error_nr="");
 
