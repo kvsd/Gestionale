f6978f99c29a7bcc3b354e3d92de7278792a9579
kvsd <kvsd@desktop>
2014-02-02 15:07:03 +0100

    Corretto problema di visualizzazione delle date
    Impostando il locale esplicitamente, le viste mostrano le
    date nel formato corretto.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     QCoreApplication::setOrganizationName("Test");
     QCoreApplication::setApplicationName("Gestionale");
-
+    QLocale::setDefault(QLocale(QLocale::Italian, QLocale::Italy));
     MainWindow w;
     w.show();
     
