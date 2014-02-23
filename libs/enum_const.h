6af9b689a8ed7d636d1358cd780748cd491e067f
kvsd <kvsd@desktop>
2014-02-23 14:06:45 +0100

    Aggiunte costanti nome tabelle database

--- libs/enum_const.h
+++ libs/enum_const.h
@@ -1,6 +1,8 @@
 #ifndef ENUM_CONST_H
 #define ENUM_CONST_H
 
+#include <QString>
+
 namespace agenti {
     enum columns {COL_ID,
                   COL_NOME,
@@ -29,5 +31,29 @@ namespace azienda {
                   COL_CCIAA};
 }
 
+namespace table {
+    const QString AGENTI = "agenti";
+    const QString AGENZIA = "agenzia";
+    const QString ANAGRAFICA = "anagrafica";
+    const QString ASPETTO_BENI = "aspetto_beni";
+    const QString AZIENDA = "azienda";
+    const QString BANCA = "banca";
+    const QString CAP = "cap";
+    const QString CASUALE_TRASPORTO = "casuale_trasporto";
+    const QString CATEGORIA_MERCE = "cat_merce";
+    const QString CITTA = "citta";
+    const QString CODICE_IVA = "cod_iva";
+    const QString LISTINO_STORICO = "listino_storico";
+    const QString MAGAZZINO = "magazzino";
+    const QString MARCA = "marca";
+    const QString PROVINCIA = "provincia";
+    const QString SEDE_MAGAZZINO = "sede_magazzino";
+    const QString STATO = "stato";
+    const QString TIPO_DITTA = "tipo_ditta";
+    const QString TIPO_PAGAMENTO = "tipo_pagamento";
+    const QString UNITA_MISURA = "unita_misura";
+    const QString USER_DB = "user_db";
+    const QString VETTORE = "vettore";
+}
 
 #endif // ENUM_CONST_H
