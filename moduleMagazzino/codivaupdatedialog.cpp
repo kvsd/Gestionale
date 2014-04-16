e18aa85a6083999394ed8b8ceaf7540c0f130b79
kvsd@fw21z <f.barbarino@gmail.com>
2014-04-16 18:31:47 +0200

    Refactoring del codice

--- moduleMagazzino/codivaupdatedialog.cpp
+++ moduleMagazzino/codivaupdatedialog.cpp
@@ -30,19 +30,19 @@ void codIvaUpdateDialog::initModel(void)
 void codIvaUpdateDialog::initComboBox(void)
 {
     ui->oldCodIvaComboBox->setModel(oldIvaModel);
-    ui->oldCodIvaComboBox->setModelColumn(magazzino::COL_DESCR);
+    ui->oldCodIvaComboBox->setModelColumn(magazzino::COL_MG_DESCR);
 
     ui->newCodIvaComboBox->setModel(newIvaModel);
-    ui->newCodIvaComboBox->setModelColumn(magazzino::COL_DESCR);
+    ui->newCodIvaComboBox->setModelColumn(magazzino::COL_MG_DESCR);
 }
 
 void codIvaUpdateDialog::updateIva(void)
 {
-    QString oldIvaId = oldIvaModel->index(ui->oldCodIvaComboBox->currentIndex(), magazzino::COL_ID).data().toString();
-    QString newIvaId = oldIvaModel->index(ui->newCodIvaComboBox->currentIndex(), magazzino::COL_ID).data().toString();
+    QString oldIvaId = oldIvaModel->index(ui->oldCodIvaComboBox->currentIndex(), magazzino::COL_MG_ID).data().toString();
+    QString newIvaId = oldIvaModel->index(ui->newCodIvaComboBox->currentIndex(), magazzino::COL_MG_ID).data().toString();
 
-    int oldIvastr = oldIvaModel->index(ui->oldCodIvaComboBox->currentIndex(), magazzino::COL_DESCR).data().toInt();
-    int newIvastr = newIvaModel->index(ui->newCodIvaComboBox->currentIndex(), magazzino::COL_DESCR).data().toInt();
+    int oldIvastr = oldIvaModel->index(ui->oldCodIvaComboBox->currentIndex(), magazzino::COL_MG_DESCR).data().toInt();
+    int newIvastr = newIvaModel->index(ui->newCodIvaComboBox->currentIndex(), magazzino::COL_MG_DESCR).data().toInt();
 
     if (!showDialogWarning(this, "Attenzione", MSG023.arg(oldIvastr).arg(newIvastr))) {
         return;
@@ -50,43 +50,53 @@ void codIvaUpdateDialog::updateIva(void)
 
     QSqlQuery query;
     query.prepare(magazzino::SELECT_ARTICOLI_FROM_IVA);
-    query.bindValue(":cod_iva", oldIvaId);
+    query.bindValue(magazzino::PH_COD_IVA, oldIvaId);
     query.exec();
     while (query.next()) {
-        QString id = query.value(magazzino::COL_ID).toString();
+        QString id = query.value(magazzino::COL_MG_ID).toString();
         QString data = QDate::currentDate().toString("dd/MM/yyyy");
-        QString quantita = query.value(magazzino::COL_QUANTITA).toString();
-        QString prezzo_acquisto = query.value(magazzino::COL_PREZZO_FATTURA).toString();
-        QString sconto = query.value(magazzino::COL_SCONTO).toString();
-        QString ricarico = query.value(magazzino::COL_RICARICO).toString();
-        QString fattura = query.value(magazzino::COL_FATTURA).toString();
-        double imponibile = query.value(magazzino::COL_PRZ_ACQUISTO).toDouble();
-        double iva = imponibile*newIvastr/100.0;
-        double prezzo_finito = imponibile+iva;
+        QString quantita = query.value(magazzino::COL_MG_QT).toString();
+        QString prezzo_fattura = query.value(magazzino::COL_MG_PRZ_FAT).toString();
+        QString sconto = query.value(magazzino::COL_MG_SCONTO).toString();
+        QString ricarico = query.value(magazzino::COL_MG_RICARICO).toString();
+        QString fattura = query.value(magazzino::COL_MG_FATTURA).toString();
+        double prezzo_acquisto = query.value(magazzino::COL_MG_PRZ_ACQ).toDouble();
+        double prezzo_ricarico = 0;
+        if (ricarico.contains("+")) {
+            QStringList ricarichi = ricarico.split("+");
+            for (int i=0; i<ricarichi.length(); i++) {
+                prezzo_ricarico = prezzo_acquisto + prezzo_acquisto*ricarichi[i].toDouble()/100;
+            }
+        }
+        else
+            prezzo_ricarico = prezzo_acquisto + (prezzo_acquisto*ricarico.toDouble()/100.0);
+
+        double iva = prezzo_ricarico*newIvastr/100.0;
+        double prezzo_finito = prezzo_ricarico+iva;
         double prezzo_vendita = prezzo_finito;
 
         QSqlQuery queryUpdateIva;
         queryUpdateIva.prepare(magazzino::UPDATE_ARTICOLI_FROM_IVA);
-        queryUpdateIva.bindValue(":id", id);
-        queryUpdateIva.bindValue(":cod_iva", newIvaId);
-        queryUpdateIva.bindValue(":iva", iva);
-        queryUpdateIva.bindValue(":prezzo_finito", prezzo_finito);
-        queryUpdateIva.bindValue(":prezzo_vendita", prezzo_vendita);
+        queryUpdateIva.bindValue(magazzino::PH_ID, id);
+        queryUpdateIva.bindValue(magazzino::PH_COD_IVA, newIvaId);
+        queryUpdateIva.bindValue(magazzino::PH_IVA, iva);
+        queryUpdateIva.bindValue(magazzino::PH_PRZ_FIN, prezzo_finito);
+        queryUpdateIva.bindValue(magazzino::PH_PRZ_VEN, prezzo_vendita);
         queryUpdateIva.exec();
 
         QSqlQuery queryInsertStorico;
         queryInsertStorico.prepare(magazzino::INSERT_STORICO);
-        queryInsertStorico.bindValue(":id_articolo", id);
-        queryInsertStorico.bindValue(":data_arrivo", data);
-        queryInsertStorico.bindValue(":quantita", quantita);
-        queryInsertStorico.bindValue(":prezzo_acquisto", prezzo_acquisto);
-        queryInsertStorico.bindValue(":sconto_fornitore", sconto);
-        queryInsertStorico.bindValue(":ricarico", ricarico);
-        queryInsertStorico.bindValue(":imponibile", imponibile);
-        queryInsertStorico.bindValue(":iva", iva);
-        queryInsertStorico.bindValue(":prezzo_finito", prezzo_finito);
-        queryInsertStorico.bindValue(":prezzo_vendita", prezzo_vendita);
-        queryInsertStorico.bindValue(":fattura", fattura);
+        queryInsertStorico.bindValue(magazzino::PH_ID_ART, id);
+        queryInsertStorico.bindValue(magazzino::PH_DATA, data);
+        queryInsertStorico.bindValue(magazzino::PH_QUANTIT, quantita);
+        queryInsertStorico.bindValue(magazzino::PH_PRZ_FAT, prezzo_fattura);
+        queryInsertStorico.bindValue(magazzino::PH_SCONTO, sconto);
+        queryInsertStorico.bindValue(magazzino::PH_RICARIC, ricarico);
+        queryInsertStorico.bindValue(magazzino::PH_PRZ_ACQ, prezzo_acquisto);
+        queryInsertStorico.bindValue(magazzino::PH_IVA, iva);
+        queryInsertStorico.bindValue(magazzino::PH_PRZ_FIN, prezzo_finito);
+        queryInsertStorico.bindValue(magazzino::PH_PRZ_VEN, prezzo_vendita);
+        queryInsertStorico.bindValue(magazzino::PH_FATTURA, fattura);
         queryInsertStorico.exec();
     }
 }
