dc7f3dec89069eccdeb272969bd1590f2353a129
kvsd@fw21z <f.barbarino@gmail.com>
2014-06-13 17:05:46 +0200

    Aggiunti controlli per configurare la stampa

--- moduleMagazzino/configprintdialog.cpp
+++ moduleMagazzino/configprintdialog.cpp
@@ -31,11 +31,17 @@ void ConfigPrintDialog::initComboBoxs()
     ui->col2ComboBox->addItems(cols);
     ui->col3ComboBox->addItems(cols);
     ui->col4ComboBox->addItems(cols);
+
+    ui->ordineCol1ComboBox->addItems(cols);
+    ui->ordineCol2ComboBox->addItems(cols);
+    ui->ordineCol3ComboBox->addItems(cols);
+    ui->ordineCol4ComboBox->addItems(cols);
 }
 
 void ConfigPrintDialog::loadSettings()
 {
     qDebug() << "ConfigPrintDialog::loadSettings()";
+    //Configura listino
     QString col1 = settings.value(magazzino::LISTINO_COL1, magazzino::CMP_COD_ART).toString();
     QString col2 = settings.value(magazzino::LISTINO_COL2, magazzino::CMP_DESCR).toString();
     QString col3 = settings.value(magazzino::LISTINO_COL3, magazzino::CMP_PRZ_ACQ).toString();
@@ -50,11 +56,28 @@ void ConfigPrintDialog::loadSettings()
     ui->col2ComboBox->setCurrentIndex(idCol2);
     ui->col3ComboBox->setCurrentIndex(idCol3);
     ui->col4ComboBox->setCurrentIndex(idCol4);
+
+    //Configura ordine
+    QString ordineCol1 = settings.value(magazzino::ORDINE_COL1, magazzino::CMP_COD_ART).toString();
+    QString ordineCol2 = settings.value(magazzino::ORDINE_COL2, magazzino::CMP_DESCR).toString();
+    QString ordineCol3 = settings.value(magazzino::ORDINE_COL3, magazzino::CMP_QT).toString();
+    QString ordineCol4 = settings.value(magazzino::ORDINE_COL4, magazzino::CMP_SCORTA).toString();
+
+    idCol1 = ui->ordineCol1ComboBox->findText(ordineCol1);
+    idCol2 = ui->ordineCol2ComboBox->findText(ordineCol2);
+    idCol3 = ui->ordineCol3ComboBox->findText(ordineCol3);
+    idCol4 = ui->ordineCol4ComboBox->findText(ordineCol4);
+
+    ui->ordineCol1ComboBox->setCurrentIndex(idCol1);
+    ui->ordineCol2ComboBox->setCurrentIndex(idCol2);
+    ui->ordineCol3ComboBox->setCurrentIndex(idCol3);
+    ui->ordineCol4ComboBox->setCurrentIndex(idCol4);
 }
 
 void ConfigPrintDialog::save()
 {
     qDebug() << "ConfigPrintDialog::save()";
+    //Configura listino
     QString col1 = ui->col1ComboBox->currentText();
     QString col2 = ui->col2ComboBox->currentText();
     QString col3 = ui->col3ComboBox->currentText();
@@ -64,4 +87,15 @@ void ConfigPrintDialog::save()
     settings.setValue(magazzino::LISTINO_COL2, col2);
     settings.setValue(magazzino::LISTINO_COL3, col3);
     settings.setValue(magazzino::LISTINO_COL4, col4);
+
+    //Configura ordine
+    QString ordineCol1 = ui->ordineCol1ComboBox->currentText();
+    QString ordineCol2 = ui->ordineCol2ComboBox->currentText();
+    QString ordineCol3 = ui->ordineCol3ComboBox->currentText();
+    QString ordineCol4 = ui->ordineCol4ComboBox->currentText();
+
+    settings.setValue(magazzino::ORDINE_COL1, ordineCol1);
+    settings.setValue(magazzino::ORDINE_COL2, ordineCol2);
+    settings.setValue(magazzino::ORDINE_COL3, ordineCol3);
+    settings.setValue(magazzino::ORDINE_COL4, ordineCol4);
 }
