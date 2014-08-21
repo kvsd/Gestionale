b2d9f8e8a44e92e20002639505f684f16ae4e277
kvsd <f.barbarino@gmail.com>
2014-08-21 18:19:09 +0200

    Corretto il problema della cancellazione del logo.

--- moduleAzienda/aziendadialog.cpp
+++ moduleAzienda/aziendadialog.cpp
@@ -247,6 +247,7 @@ void AziendaDialog::clearForm(void)
     ui->cb_cap->setCurrentIndex(0);
     ui->cb_stato->setCurrentIndex(0);
     ui->im_logo->clear();
+    logo = 0;
 }
 
 void AziendaDialog::copy_prt_iva(void)
