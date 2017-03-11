d5f976076eb9505af70448da3b85899d69399abc
kvsd <f.barbarino@gmail.com>
2017-03-11 12:53:45 +0100

    Refactoring della classe OptionsAnagraficaDialog.

--- moduleAnagrafica/optionsanagraficadialog.cpp
+++ moduleAnagrafica/optionsanagraficadialog.cpp
@@ -1,16 +1,16 @@
 #include "optionsanagraficadialog.h"
 #include "ui_optionsanagraficadialog.h"
 
-OptionsAnagraficaDialog::OptionsAnagraficaDialog(CustomModel *model, QWidget *parent) :
-    QDialog(parent),
+OptionsAnagraficaDialog::OptionsAnagraficaDialog(QWidget *parent) :
+    OptionsDialog(parent),
+    anagraficaModel(new CustomModel(this)),
     ui(new Ui::OptionsAnagraficaDialog)
 {
     qDebug() << "OptionsAnagraficaDialog()";
     ui->setupUi(this);
 
-    anagraficaModel = model;
-    prepareMap();
-    populateAnagraficaList();
+    anagraficaModel->setQuery(anagrafica::SELECT_ALL);
+    populateList(ui->lw_column_anagrafica, anagraficaModel);
     loadConfig();
 }
 
@@ -20,98 +20,32 @@ OptionsAnagraficaDialog::~OptionsAnagraficaDialog()
     delete ui;
 }
 
-void OptionsAnagraficaDialog::prepareMap()
-{
-    qDebug() << "OptionsAnagraficaDialog::prepareMap()";
-    anagraficaNameCols = prepareMapsFromModel(anagraficaModel);
-}
-
-void OptionsAnagraficaDialog::populateAnagraficaList(void)
-{
-    qDebug() << "OptionsAnagraficaDialog::populateAnagraficaList()";
-    for (QMap<int,QString>::Iterator i = anagraficaNameCols.begin(); i!=anagraficaNameCols.end(); i++) {
-        ui->lw_column_anagrafica->insertItem(i.key(), i.value());
-        QListWidgetItem *col = ui->lw_column_anagrafica->item(i.key());
-        col->setFlags(Qt::ItemIsSelectable | Qt::ItemIsUserCheckable | Qt::ItemIsEnabled);
-        col->setCheckState(Qt::Checked);
-    }
-}
-
 void OptionsAnagraficaDialog::saveConfig(void)
 {
     qDebug() << "OptionsAnagraficaDialog::saveConfig()";
-    settings.beginGroup(anagrafica::ANGRFC_STATUS);
-    for (QMap<int,QString>::Iterator i=anagraficaNameCols.begin(); i!=anagraficaNameCols.end(); i++) {
-        QListWidgetItem *col = ui->lw_column_anagrafica->item(i.key());
-        int checkState = col->checkState()==Qt::Checked ? 1 : 0;
-        settings.setValue(QVariant(i.key()).toString(), checkState);
-    }
-    settings.endGroup();
+    //Salva la visibilità delle colonne
+    saveVisibility(ui->lw_column_anagrafica, anagrafica::ANGRFC_STATUS);
 
-    settings.beginGroup(anagrafica::ANGRFC_COLORS);
-    for (QMap<int,QString>::Iterator i=anagraficaNameCols.begin(); i!=anagraficaNameCols.end(); i++) {
-        QListWidgetItem *col = ui->lw_column_anagrafica->item(i.key());
-        QColor color = col->background().color();
-        if (color == QColor(Qt::transparent))
-            settings.setValue(QVariant(i.key()).toString(), "-1");
-        else
-            settings.setValue(QVariant(i.key()).toString(), color.name());
-    }
-    settings.endGroup();
+    //Salva il colore di sfondo delle colonne
+    saveBgColor(ui->lw_column_anagrafica, anagrafica::ANGRFC_COLORS);
 }
 
 void OptionsAnagraficaDialog::loadConfig(void)
 {
     qDebug() << "OptionsAnagraficaDialog::loadConfig()";
-    settings.beginGroup(anagrafica::ANGRFC_STATUS);
-    for (QMap<int,QString>::Iterator i=anagraficaNameCols.begin(); i!=anagraficaNameCols.end(); i++) {
-        QListWidgetItem *col = ui->lw_column_anagrafica->item(i.key());
-        int status = settings.value(QVariant(i.key()).toString(), 1).toInt();
-        col->setCheckState(status==1 ? Qt::Checked : Qt::Unchecked);
-    }
-    settings.endGroup();
+    //Carica la visibilita delle colonne
+    loadVisibility(ui->lw_column_anagrafica, anagrafica::ANGRFC_STATUS);
 
-    QColor color;
-    settings.beginGroup(anagrafica::ANGRFC_COLORS);
-    for (QMap<int,QString>::Iterator i=anagraficaNameCols.begin(); i!=anagraficaNameCols.end(); i++) {
-        QListWidgetItem *col = ui->lw_column_anagrafica->item(i.key());
-        QString colorStr = settings.value(QVariant(i.key()).toString(), "-1").toString();
-        if (colorStr == "-1")
-            color = QColor(Qt::transparent);
-        else
-            color.setNamedColor(colorStr);
-
-        col->setBackground(QBrush(color));
-    }
-    settings.endGroup();
+    //Carica il colore di sfondo delle colonne
+    loadBgColor(ui->lw_column_anagrafica, anagrafica::ANGRFC_COLORS);
 }
 
 void OptionsAnagraficaDialog::restoreToDefault(void)
 {
     qDebug() << "OptionsAnagraficaDialog::restoreToDefault()";
-    settings.beginGroup(anagrafica::ANGRFC_STATUS);
-    for (QMap<int,QString>::Iterator i=anagraficaNameCols.begin(); i!=anagraficaNameCols.end(); i++) {
-        QListWidgetItem *col = ui->lw_column_anagrafica->item(i.key());
-        col->setCheckState(Qt::Checked);
-        settings.setValue(QVariant(i.key()).toString(), 1);
-    }
-    settings.endGroup();
 
-    settings.beginGroup(anagrafica::ANGRFC_COLORS);
-    for (QMap<int,QString>::Iterator i=anagraficaNameCols.begin(); i!=anagraficaNameCols.end(); i++) {
-        settings.setValue(QVariant(i.key()).toString(), "-1");
-    }
-    settings.endGroup();
+    restoreListWidget(ui->lw_column_anagrafica);
+    saveConfig();
+    loadConfig();
 }
 
-void OptionsAnagraficaDialog::setColumnColor(QModelIndex index)
-{
-    qDebug() << "OptionsAnagraficaDialog::setColumnColor()";
-    QListWidgetItem *col = ui->lw_column_anagrafica->item(index.row());
-    QColorDialog dlg(this);
-    bool ok = dlg.exec();
-    if (ok) {
-        QColor color = dlg.selectedColor();
-        col->setBackgroundColor(color);
-    }
-}
--- moduleAnagrafica/optionsanagraficadialog.h
+++ moduleAnagrafica/optionsanagraficadialog.h
@@ -1,42 +1,35 @@
 #ifndef OPTIONSANAGRAFICADIALOG_H
 #define OPTIONSANAGRAFICADIALOG_H
 
-#include "libs/utils.h"
 #include "libs/custommodel.h"
+#include "libs/optionsdialog.h"
 #include "anagrafica_const.h"
-#include <QDialog>
 #include <QMap>
 #include <QDebug>
-#include <QSettings>
-#include <QColorDialog>
-#include <QModelIndex>
 
 namespace Ui {
 class OptionsAnagraficaDialog;
 }
 
-class OptionsAnagraficaDialog : public QDialog
+class OptionsAnagraficaDialog : public OptionsDialog
 {
     Q_OBJECT
 
 public:
-    explicit OptionsAnagraficaDialog(CustomModel *model, QWidget *parent = 0);
+    explicit OptionsAnagraficaDialog(QWidget *parent = 0);
     ~OptionsAnagraficaDialog();
 
 private:
+    CustomModel *anagraficaModel;
     Ui::OptionsAnagraficaDialog *ui;
     QMap <int, QString> anagraficaNameCols;
-    QSettings settings;
-    CustomModel *anagraficaModel;
 
-    void prepareMap(void);
     void populateAnagraficaList(void);
 
 public slots:
     void saveConfig(void);
     void loadConfig(void);
     void restoreToDefault(void);
-    void setColumnColor(QModelIndex);
 };
 
 #endif // OPTIONSANAGRAFICADIALOG_H
