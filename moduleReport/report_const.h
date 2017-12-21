8eeac43b6f11acd857979635c713b0f54191f8a9
kvsd <f.barbarino@gmail.com>
2017-12-21 19:30:37 +0100

    Modifica a report_const.h
    *Rimosse diverse variabili non usate
    *Aggiunti i namespace settings e CPD

--- moduleReport/report_const.h
+++ moduleReport/report_const.h
@@ -3,8 +3,6 @@
 
 #include "magazzino_const.h"
 #include <QString>
-#include <QRect>
-#include <QMap>
 
 namespace sql {
     //Filtro fornitore.
@@ -18,13 +16,6 @@ namespace sql {
     //Filtro fornitore e numero fattura.
     const QString FILTER_FATTURA = " AND rag_sociale=:rag_sociale"
             " AND fattura ILIKE :fattura ORDER BY descr";
-}
-
-namespace report {
-
-    //SETTINGS
-    const QString listinoCols = "Listino.cols";
-    const QString ordineCols = "Ordine.cols";
 
     //Select utilizzata per la stampa dell'inventario
     const QString SELECT_INVENTARIO = "SELECT * FROM vw_inventario WHERE \"Quantità\"!=0";
@@ -33,38 +24,21 @@ namespace report {
     const QString SQL_INVENTARIO_TOT =
             QString::fromUtf8("SELECT sum(\"SubTotale\") AS \"Totale\" "
                               "FROM vw_inventario");
+}
 
-    const QString LISTINO = "Listino";
-    const QString INVENTARIO = "Inventario";
-    const QString ORDINE = "Ordine";
+namespace settings {
+    //SETTINGS
+    const QString listinoCols = "Listino.cols";
+    const QString ordineCols = "Ordine.cols";
+}
 
-        //CMP Nome Campo Tabella sql
-        const QString CMP_ID      = "Id";
-        const QString CMP_DESCR   = "Descrizione";
-        const QString CMP_FORNIT  = "Fornitore";
-        const QString CMP_MARCA   = "Marca";
-        const QString CMP_MODELLO = "Modello";
-        const QString CMP_COD_ART = "Cod.Articolo";
-        const QString CMP_COD_FOR = "Cod.Fornitore";
-        const QString CMP_COD_EAN = "Cod.EAN";
-        const QString CMP_MERCE   = "Cat.Merce";
-        const QString CMP_COD_IVA = "Cod.IVA";
-        const QString CMP_UM      = "UM";
-        const QString CMP_SCORTA  = "Scorta Minima";
-        const QString CMP_QT      = QString::fromUtf8("Quantità");
-        const QString CMP_PRZ_FAT = "Prezzo Fattura";
-        const QString CMP_SCONTO  = "Sconto Fornitore";
-        const QString CMP_PRZ_ACQ = "Prezzo Acquisto";
-        const QString CMP_RICAR   = "Ricarico";
-        const QString CMP_IVA     = "Iva";
-        const QString CMP_PRZ_FIN = "Prezzo Finito";
-        const QString CMP_PRZ_VEN = QString::fromUtf8("Prezzo Vendità");
-        const QString CMP_FATTURA = "Nr.Fattura";
-        const QString CMP_DATA    = "Data Arrivo";
-        const QString CMP_SEDE    = "Sede Magazzino";
-        const QString CMP_NOTE    = "Note";
-        const QString CMP_ID_ART  = "Id Articolo";
-        const QString CMP_SUBTOT = "SubTotale";
-
+namespace CPD {
+    //Colonne ConfigPrintDialog.TableWidget
+    enum col{
+        DESCR = 0,
+        STRETCH = 1,
+        VIEW = 2,
+        ALIGN = 3
+    };
 }
 #endif // REPORT_CONST_H
