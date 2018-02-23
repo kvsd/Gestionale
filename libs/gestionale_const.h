ad86c8ee458264e9ee33f1d3978f17ddff566548
kvsd <f.barbarino@gmail.com>
2018-02-23 19:31:06 +0100

    Creato il namespace colview.

--- libs/gestionale_const.h
+++ libs/gestionale_const.h
@@ -155,5 +155,62 @@ namespace coldb {
 
 namespace colview {
 //Nome colonne tableview/tablewidget
+//const QString AGENZIA = "agenzia";
+//const QString BANCA = "banca";
+//const QString CEL = "cel";
+//const QString CLIENTE = "cliente";
+const QString CODICE_ARTICOLO = "Cod.Articolo";
+const QString CODICE_BARRE = "Cod.EAN";
+//const QString CODICE_FISCALE = "cod_fisc";
+const QString CODICE_FORNITORE = "Cod.Fornitore";
+const QString CODICE_IVA = "Cod.IVA";
+//const QString COGNOME = "cognome";
+//const QString CONTO = "conto";
+const QString DATA = "Data";
+const QString DATA_ARRIVO = "Data Arrivo";
+const QString DESCRIZIONE = "Descrizione";
+//const QString DESTINAZIONE_MERCE = "dest_merce";
+//const QString EMAIL = "email";
+const QString ENT_BANCA = "Entrata Banca";
+const QString ENT_CASSA = "Entrata Cassa";
+//const QString FAX = "fax";
+const QString FATTURA = "Nr.Fattura";
+const QString FORNITORE = "Fornitore";
+//const QString IBAN = "iban";
+const QString ID = "Id";
+//const QString ID_AGENTE = "id_agente";
+//const QString ID_ARTICOLO = "id_articolo";
+//const QString ID_CAP = "id_cap";
+//const QString ID_CITTA = "id_citta";
+//const QString ID_FORNITORE = "id_fornitore";
+//const QString ID_MARCA = "id_marca";
+//const QString ID_MERCE = "id_merce";
+//const QString ID_PROVINCIA = "id_provincia";
+//const QString ID_SEDE_MAGAZZINO = "id_sede_magazzino";
+//const QString ID_STATO = "id_stato";
+//const QString ID_TIPO_DITTA = "id_tipo_ditta";
+//const QString ID_UNITA = "id_unita_misura";
+//const QString INDIRIZZO = "indirizzo";
+const QString IVA = "Iva";
+const QString MODELLO = "Modello";
+//const QString NOME = "nome";
+const QString NOTE = "Note";
+//const QString PARTITA_IVA = "prt_iva";
+const QString PREZZO_ACQUISTO = "Prezzo Acquisto";
+const QString PREZZO_FATTURA = "Prezzo Fattura";
+const QString PREZZO_FINITO = "Prezzo Finito";
+const QString PREZZO_VENDITA = "Prezzo Vendità";
+const QString PREZZO_VENDITA_B = "Prezzo Vendità B";
+const QString QUANTITA = "Quantità";
+//const QString RAGIONE_SOCIALE = "rag_sociale";
+const QString RICARICO = "Ricarico";
+const QString SCONTO_FORNITORE = "Sconto Fornitore";
+const QString SCORTA_MINIMA = "Scorta Minima";
+//const QString SITO_WEB = "sito_web";
+//const QString SWIFT = "swift";
+//const QString TEL = "tel";
+const QString USC_BANCA = "Uscita Banca";
+const QString USC_CASSA = "Uscita Cassa";
 }
+
 #endif // GESTIONALE_CONST_H
