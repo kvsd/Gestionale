7ef8d777433d26db087a3daeb6b2660dd4cb31f7
kvsd <f.barbarino@gmail.com>
2019-01-18 16:08:08 +0100

    Aggiunte query in documenti_const.h

--- moduleDocumenti/documenti_const.h
+++ moduleDocumenti/documenti_const.h
@@ -11,5 +11,26 @@ namespace documenti {
     const QString SELECT_CLIENTE = "SELECT id, concat(rag_sociale, cognome, nome) AS rag_sociale "
                                    "FROM anagrafica";
 
+    //Utilizzato in DocumentiAddDialog.
+    const QString INSERT_DOC = "INSERT INTO documenti(id_cliente, id_tipo_documento, "
+                               "                      id_moneta, data, years, nr_documento, "
+                               "                      importo_tot, arrotondamento, casuale) "
+                               "VALUES(:id_cliente, :id_tipo_documento, :id_moneta, :data, "
+                               "       :years, 'FA'||nextval('fatt_seq'), :importo_tot, "
+                               "       :arrotondamento, :casuale) "
+                               "RETURNING id";
+
+     const QString INSERT_DET = "INSERT INTO documenti_det (id_documento, linea, cod_articolo, "
+                                "                           descr, quantita, um, prezzo_unitario, "
+                                "                           prezzo_totale, aliquota_iva, rif) "
+                                "VALUES(:id_documento, :linea, :cod_articolo, "
+                                "       :descr, :quantita, :um, :prezzo_unitario, "
+                                "       :prezzo_totale, :aliquota_iva, :rif)";
+
+     const QString INSERT_IVA = "INSERT INTO documenti_iva(id_documento, aliquota_iva, "
+                                "                          imponibile, imposta)"
+                                "VALUES (:id_documento, :aliquota_iva, "
+                                "        :imponibile, :imposta)";
+
 }
 #endif // DOCUMENTI_CONST_H
